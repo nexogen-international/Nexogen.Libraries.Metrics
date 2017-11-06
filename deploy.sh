3aa17c5ca561466a8397c752e280412dfd3f75e3
Adam Hoka <adam.hoka@gmail.com>
2017-11-06 11:15:25 +0100

    Fix build on Unix

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,10 @@ else
 	ARGS="-c Release /p:PackageVersionNumber=${TRAVIS_TAG#v}"
 fi
 
-dotnet msbuild /t:Restore;Pack ./Nexogen.Libraries.Metrics $ARGS
-dotnet msbuild /t:Restore;Pack ./Nexogen.Libraries.Metrics.Extensions $ARGS
-dotnet msbuild /t:Restore;Pack ./Nexogen.Libraries.Metrics.Prometheus $ARGS
-dotnet msbuild /t:Restore;Pack ./Nexogen.Libraries.Metrics.Prometheus.AspCore $ARGS
-dotnet msbuild /t:Restore;Pack ./Nexogen.Libraries.Metrics.Prometheus.PushGateway $ARGS
+dotnet msbuild "/t:Restore;Pack" ./Nexogen.Libraries.Metrics $ARGS
+dotnet msbuild "/t:Restore;Pack" ./Nexogen.Libraries.Metrics.Extensions $ARGS
+dotnet msbuild "/t:Restore;Pack" ./Nexogen.Libraries.Metrics.Prometheus $ARGS
+dotnet msbuild "/t:Restore;Pack" ./Nexogen.Libraries.Metrics.Prometheus.AspCore $ARGS
+dotnet msbuild "/t:Restore;Pack" ./Nexogen.Libraries.Metrics.Prometheus.PushGateway $ARGS
 
 dotnet nuget push **/*.nupkg -s https://nuget.org -k $NUGET_API_KEY
