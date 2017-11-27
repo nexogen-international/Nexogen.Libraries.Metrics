2998877c56583f125b0409c92ba1ea5de8bc14a6
Ádám Hóka <hoka.adam@nexogen.hu>
2017-11-27 14:41:43 +0100

    Add some comments

--- Nexogen.Libraries.Metrics.Prometheus.Standalone/PrometheusServerException.cs
+++ Nexogen.Libraries.Metrics.Prometheus.Standalone/PrometheusServerException.cs
@@ -4,8 +4,12 @@ using System.Text;
 
 namespace Nexogen.Libraries.Metrics.Prometheus.Standalone
 {
+    /// <summary>
+    /// Generic exception to hide internal exceptions from PrometheusServer.
+    /// </summary>
     public class PrometheusServerException : Exception
     {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public PrometheusServerException()
         {
         }
@@ -17,5 +21,6 @@ namespace Nexogen.Libraries.Metrics.Prometheus.Standalone
         public PrometheusServerException(string message, Exception innerException) : base(message, innerException)
         {
         }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     }
 }
