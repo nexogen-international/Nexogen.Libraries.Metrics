4a614048055b5d3ebb4423e6ef63f3cf8ce84110
Ádám Hóka <hoka.adam@nexogen.hu>
2017-11-28 12:20:08 +0100

    Add builder.

--- Nexogen.Libraries.Metrics.Prometheus.Standalone/PrometheusServer.cs
+++ Nexogen.Libraries.Metrics.Prometheus.Standalone/PrometheusServer.cs
@@ -4,47 +4,34 @@ using System.Text;
 using System.Net.Http;
 using System.Net;
 using Nexogen.Libraries.Metrics.Prometheus;
+using System.Threading.Tasks;
 
 namespace Nexogen.Libraries.Metrics.Prometheus.Standalone
 {
     /// <summary>
     /// A simple Prometheus metric server using an embedded HTTP server.
     /// </summary>
-    public class PrometheusServer : IDisposable
+    internal class PrometheusServer : IDisposable
     {
-        HttpListener listener;
+        private readonly HttpListener listener;
+        private readonly IExposable metrics;
+        private readonly Action<Exception> errorHandler;
 
         /// <summary>
         /// Creates and starts a metric server.
         /// </summary>
         /// <param name="metrics">The metric to expose</param>
         /// <param name="listener">An HttpListener instance to use</param>
-        public PrometheusServer(IExposable metrics, HttpListener listener)
+        /// <param name="errorHandler">Error handling callback</param>
+        public PrometheusServer(IExposable metrics, HttpListener listener, Action<Exception> errorHandler)
         {
             this.listener = listener ?? throw new ArgumentNullException(nameof(listener));
-
-            if (metrics == null)
-            {
-                throw new ArgumentNullException(nameof(metrics));
-            }
+            this.metrics = metrics ?? throw new ArgumentNullException(nameof(metrics));
+            this.errorHandler = errorHandler;
 
             try
             {
-                this.listener.Request += async (sender, context) =>
-                {
-                    if (context.Request.HttpMethod == HttpMethods.Get)
-                    {
-                        context.Response.StatusCode = 200;
-                        context.Response.Headers.Add("Content-Type", "text/plain; version=0.0.4");
-                        await metrics.Expose(context.Response.OutputStream, ExposeOptions.Default);
-                    }
-                    else
-                    {
-                        context.Response.NotFound();
-                    }
-
-                    context.Response.Close();
-                };
+                this.listener.Request += RequestHandler;
 
                 listener.Start();
             }
@@ -54,25 +41,34 @@ namespace Nexogen.Libraries.Metrics.Prometheus.Standalone
             }
         }
 
-        /// <summary>
-        /// Creates and starts a metric server.
-        /// </summary>
-        /// <param name="metrics">The metric to expose</param>
-        /// <param name="address">The IP address to listen on</param>
-        /// <param name="port">The TCP port to bind to</param>
-        public PrometheusServer(IExposable metrics, IPAddress address, int port)
-            : this(metrics, new HttpListener(address, port))
+        private async void RequestHandler(object sender, HttpListenerRequestEventArgs context)
         {
-        }
+            try
+            {
+                if (context.Request.HttpMethod == HttpMethods.Get)
+                {
+                    context.Response.StatusCode = 200;
+                    context.Response.Headers.Add("Content-Type", "text/plain; version=0.0.4");
+                    await metrics.Expose(context.Response.OutputStream, ExposeOptions.Default);
+                }
+                else
+                {
+                    context.Response.NotFound();
+                }
 
-        /// <summary>
-        /// Creates and starts a metric server.
-        /// </summary>
-        /// <param name="metrics">The metric to expose</param>
-        /// <param name="port">The TCP port to bind to</param>
-        public PrometheusServer(IExposable metrics, int port)
-            : this(metrics, IPAddress.Parse("0.0.0.0"), port)
-        {
+                context.Response.Close();
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    errorHandler(new PrometheusServerException("Error while handling HTTP request", ex));
+                }
+                catch (Exception)
+                {
+                    // ignore exceptions thrown by user supplied callback
+                }
+            }
         }
 
         /// <summary>
